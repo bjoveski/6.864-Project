6d71915127727f9eda131fc748887ca983d82e25
Bojan Joveski <bjoveski@gmail.com>
2012-11-03 11:57:12 -0400

    fix space ident gggrrr

--- Question.py
+++ Question.py
@@ -17,23 +17,23 @@ class Question:
 
 
   def populateTitleVector(self, titleHistogram):
-  	self.titleVector = zeros(titleHistogram.vocabSize, dtype = int16)
-  	for word in Parser.Parser.getWordArray(self.title):
-  		if (word in titleHistogram.word2index):
-  			index = titleHistogram.word2index[word]
-  			self.titleVector[index] = 1
+    self.titleVector = zeros(titleHistogram.vocabSize, dtype = int16)
+    for word in Parser.Parser.getWordArray(self.title):
+      if (word in titleHistogram.word2index):
+        index = titleHistogram.word2index[word]
+        self.titleVector[index] = 1
 
   def compareTitles(self, question):
-  	vec1 = self.titleVector 
-  	vec2 = question.titleVector
-  	norm1 = inner(vec1, vec1)
-  	norm2 = inner(vec2, vec2)
-  	if (norm1 != 0 and norm2 != 0):
-		  return inner(vec1, vec2) / (math.sqrt(norm1) * math.sqrt(norm2))
-	  else:
-		  return 0
+    vec1 = self.titleVector 
+    vec2 = question.titleVector
+    norm1 = inner(vec1, vec1)
+    norm2 = inner(vec2, vec2)
+    if (norm1 != 0 and norm2 != 0):
+      return inner(vec1, vec2) / (math.sqrt(norm1) * math.sqrt(norm2))
+    else:
+      return 0
 
-	def findMostSimilar(self, questions):
+  def findMostSimilar(self, questions):
     maxSimilarity = 0
     mostSimilar = None
     for q in questions:
