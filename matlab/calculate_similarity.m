2d6ea525e9d013f7df3e4cf32f1b0b5efb81edd3
diegcif <diegcif@mit.edu>
2012-12-01 17:52:23 -0500

    tfidf

--- matlab/calculate_similarity.m
+++ matlab/calculate_similarity.m
@@ -2,17 +2,20 @@
 function [similarity] = calculate_similarity(question, questions_matrix, lambdas)
 
     s1 = cosine_score(question, questions_matrix); % vector 
- %   s2 = tfidf(question, questions_matrix);
+    s2 = tfidf_score(question, questions_matrix);
     
- %   similarity = lambdas(1) * (s1) + lambdas(2) * (s2);
-    similarity = lambdas(1) * (s1);
+    similarity = lambdas(1) * s1 + lambdas(1) * s2;
 end
 
 
-function [cos_score] =  cosine_score(question, questions_matrix)
-    cos_score = question * questions_matrix;
+function [score] =  cosine_score(question, questions_matrix)
+    score = questions_matrix * question'./ (.1+norm(question)*sqrt(sum(questions_matrix.^2,2)));
 end
 
+function [score] =  tfidf_score(question, questions_matrix)
+    idf = log(331895./(sum(questions_matrix>0)+1));
+    score = cosine_score(question.*idf, questions_matrix);
+end
 
 %% returns a single value
 function [score] = similarity_to_score(expected, similarity_vector, TOP_K)
