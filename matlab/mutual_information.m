40c93e45d61ee137e786410f211ab172fae10dce
Bojan Joveski <bjoveski@gmail.com>
2012-12-08 14:32:56 -0500

    calculates mut inf

--- matlab/mutual_information.m
+++ matlab/mutual_information.m
@@ -1,29 +1,73 @@
-function [res] = mutual_information(document_matrix)
+function [mut_inf] = mutual_information(document_matrix, THRESHOLD, NUM_ROWS)
     N = 92789; % number of documents
     num_words = 114268;
-    res = zeros(10, num_words); %sparse(num_words, num_words);
+    BATCH_SIZE = 5000;
+    mut_inf = sparse(NUM_ROWS, num_words); %sparse(num_words, num_words);
+%    save(save_file, 'res')
     
     individual_prob = individual_count(document_matrix) / N;
     
-    for w_index=1:10
+    i = 1;
+    % range = 1:50;
+    range = 1:NUM_ROWS;
+    % range = randi([1,num_words], 50);
+    % range = range(1,:);
+    tic;
+    for w_index=range
         mutual_info_prob = mutual_count(w_index, document_matrix) / N;
-        res(w_index,:) = mutual_information_row(w_index, individual_prob, mutual_info_prob);
+        [~, rows] = size(mutual_info_prob);
+        if rows > 1
+            mut_inf(w_index,:) = mutual_information_row(w_index, individual_prob, mutual_info_prob, THRESHOLD);
+        end
+        
+        % progress bar
+        if (mod(w_index, 128) == 0)
+           str = sprintf('processing %5d    frac=%2f', w_index, w_index / NUM_ROWS);
+           disp(str);
+           toc;
+           tic;
+        end
+        
+        % flush
+        if (i >= BATCH_SIZE)
+           str = sprintf('#### outputing %5d #####', w_index);
+           disp(str);
+           save_file = sprintf('mut_inf_%d_%d.mat', BATCH_SIZE, w_index);
+           save(save_file, 'mut_inf')
+           % reset vars
+           i = 0;
+           mut_inf = sparse(NUM_ROWS, num_words);
+        end
+        
+        i = i + 1;
     end
 end
 
+
+
 %% mutual info
-function [wth_row] = mutual_information_row(w_index, individual_count_prob, mutual_info_prob)
+function [wth_row] = mutual_information_row(w_index, individual_count_prob, mutual_info_prob, THRESHOLD)
     %% X_w = 1 X_u = 1
+    
     p_w = individual_count_prob(w_index);
     p_u_row = individual_count_prob;
     p_wu_row = mutual_info_prob;
+
+    %{
+    disp('####');
+    size(p_w)
+    size(p_u_row)
+    size(p_wu_row)
+    %}
     
     wth_row = calculate_summand(p_w, p_u_row, p_wu_row);
+
     
     %% X_w = 1 X_u = 0
     p_w = individual_count_prob(w_index);
     p_u_row = 1 - individual_count_prob;
     p_wu_row = individual_count_prob(w_index) - mutual_info_prob;
+
     
     wth_row = wth_row + calculate_summand(p_w, p_u_row, p_wu_row);
     
@@ -31,23 +75,29 @@ function [wth_row] = mutual_information_row(w_index, individual_count_prob, mutu
     p_w = 1 - individual_count_prob(w_index);
     p_u_row = individual_count_prob;
     p_wu_row = individual_count_prob - mutual_info_prob;
-    
+
     wth_row = wth_row + calculate_summand(p_w, p_u_row, p_wu_row);
     
     %% X_w = 0 X_u = 0
     p_w = 1 - individual_count_prob(w_index);
     p_u_row = 1 - individual_count_prob;
-    p_wu_row = (1 - individual_count_prob(w_index)) - (individual_count_prob - mutual_info_prob);
-    
+    p_wu_row = (1 + mutual_info_prob) - (individual_count_prob(w_index) + individual_count_prob);
+
     wth_row = wth_row + calculate_summand(p_w, p_u_row, p_wu_row);
-    
+
+    % clean up
+    wth_row(isnan(wth_row)) = 0;
+    wth_row = wth_row.*(wth_row > THRESHOLD);
 end
 
 
 
 function [mut_inf_row] = calculate_summand(p_w, p_u_row, p_wu_row)
-    log_prob = log(p_wu_row./(p_u_row * p_w));
+    prob = p_wu_row./(p_w * p_u_row);
+    
+    log_prob = log(prob);
     mut_inf_row = times(p_wu_row, log_prob); % maybe adding > 0.0001 or sth to dramatically reduce dimensionality
+
 end
 
 
